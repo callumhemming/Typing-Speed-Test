e4447973f03dfa152531c87356c3d7b2aaeaec2f
Callum Hemming <callumhemming@Amiras-MacBook-Pro.local>
2022-01-15 05:31:22 +0000

    Dummy fully clean

--- src/Components/Dummy/index.js
+++ src/Components/Dummy/index.js
@@ -1,43 +1,35 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useEffect, useReducer } from "react";
 
-import {reduceToLetterColour, reduceSentanceLogic, actions} from "./libs/actions.js"
+import {
+  reduceToLetterColour,
+  reduceSentanceLogic,
+  actions,
+} from "./libs/actions.js";
 
 
-//actions.GET_NEW_SENTANCE.NEW_SENTANCE
 
-export default function Dummy({
-  textInput,
-  setTextInput,
-  timeout,
-  arrayOfScores,
-  setArrayOfScores,
-}) {
-  
-  
-  const [letterColor, getLetterColour] = useReducer(reduceToLetterColour, {
-    payload: "",
-  });
-  
-  const [sentanceData, getNewSentance] = useReducer(reduceSentanceLogic, actions.GET_NEW_SENTANCE.BOOTSTRAP_STATE());
-    
-    useEffect(()=>{ //Populate sentanceData
-      getNewSentance({type:actions.GET_NEW_SENTANCE.NEW_SENTANCE})
-    },[])
-    
+export default function Dummy({ textInput, setTextInput, arrayOfScores }) {
+  const [sentanceData, getNewSentance] = useReducer(
+    reduceSentanceLogic,
+    actions.GET_NEW_SENTANCE.BOOTSTRAP_STATE()
+  );
 
-  //Render sentence and change background colour depending on user input(UGLY)
-  const display = sentanceData.currentSentance.split.map((v, i) => {
-    const { payload } = getLetterColour({
-      characterAtIndex: v,
-      usersCharacterAtIndex: textInput[i],
-    });
-    return payload;
-  });
-
+  const [letterColor, setLetterColour] = useReducer(reduceToLetterColour, []);
+  useEffect(() => {
+    //Populate sentanceData
+    getNewSentance({ type: actions.GET_NEW_SENTANCE.NEW_SENTANCE });
+  }, []);
 
   useEffect(() => {
-    if (textInput.length == display.length) {
-      getNewSentance({type:actions.GET_NEW_SENTANCE.NEW_SENTANCE})
+    setLetterColour({
+      currentSentance: sentanceData.currentSentance.split,
+      textInput: textInput,
+    });
+  }, [textInput]);
+
+  useEffect(() => {
+    if (textInput.length == letterColor.length) {
+      getNewSentance({ type: actions.GET_NEW_SENTANCE.NEW_SENTANCE });
       setTextInput("");
       console.log(arrayOfScores);
     } else {
@@ -45,7 +37,7 @@ export default function Dummy({
     }
   }, [textInput]);
 
-
-
-  return <div style={{ display: "flex", flexDirection: "row" }}>{display}</div>;
+  return (
+    <div style={{ display: "flex", flexDirection: "row" }}>{letterColor}</div>
+  );
 }
