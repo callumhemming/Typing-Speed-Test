7f4489a793154f8c60d55915c953b58d8e64ecd8
Callum Hemming <callumhemming@Amiras-MacBook-Pro.local>
2022-01-15 01:24:30 +0000

    Cleaned up Dummy/actions

--- src/Components/Dummy/index.js
+++ src/Components/Dummy/index.js
@@ -1,45 +1,9 @@
 import React, { useState, useEffect, useReducer } from "react";
-import { sentances } from "./libs/data.jsm";
-import { reduceToLetterColour } from "./libs/actions.js";
-// import {reduceToLetterColour, reduceSentanceLogic} from "./libs/actions.js"
+
+import {reduceToLetterColour, reduceSentanceLogic, actions} from "./libs/actions.js"
 
 
-  // state = {
-  //   previousSentances: [
-  //     { Text: "Sentance", Length: 1 },
-  //     { Text: "Sentance", Length: 1 },
-  //   ],
-  //   totalLength: 1,
-  //   currentSentance: { split: ["", "", "", ""], joined: "" },
-  // };
-
-function reduceSentanceLogic(state, action) {
-      const { previousSentances, currentSentance, totalLength } = state;
-      const { type } = action;
-
-      const getUniqueSentance = (history) => {
-        const generateNewSentance =
-          sentances[Math.floor(Math.random() * sentances.length)];
-        const reduceHistory = history.reduce((a, b) => {
-          return [...a, b.text];
-        }, []);
-
-        if (reduceHistory.indexOf(generateNewSentance) != -1) {
-          return getUniqueSentance(history);
-        } else {
-          return generateNewSentance;
-        }
-      };
-
-      const oldSentance = {
-        text: currentSentance.joined,
-        length: currentSentance.joined.length,
-      };
-      const newSentanceHistory = [...previousSentances, oldSentance];
-
-      const newSentance = getUniqueSentance(newSentanceHistory);
-
-}
+//actions.GET_NEW_SENTANCE.NEW_SENTANCE
 
 export default function Dummy({
   textInput,
@@ -48,25 +12,21 @@ export default function Dummy({
   arrayOfScores,
   setArrayOfScores,
 }) {
-  //useEffect
-  // const [sentances, dispatch] = useReducer(reducer, sentances)
-
+  
+  
   const [letterColor, getLetterColour] = useReducer(reduceToLetterColour, {
     payload: "",
   });
-
-  const [sentanceData, getSentanceData] = useReducer(reduceSentanceLogic, {
-    payload: "",
-  });
-
-  // const dummyData =
-
-  const [randomSent, setRandomSent] = useState(
-    dummy[Math.floor(Math.random() * dummy.length)].split("")
-  );
+  
+  const [sentanceData, getNewSentance] = useReducer(reduceSentanceLogic, actions.GET_NEW_SENTANCE.BOOTSTRAP_STATE());
+    
+    useEffect(()=>{ //Populate sentanceData
+      getNewSentance({type:actions.GET_NEW_SENTANCE.NEW_SENTANCE})
+    },[])
+    
 
   //Render sentence and change background colour depending on user input(UGLY)
-  const display = randomSent.map((v, i) => {
+  const display = sentanceData.currentSentance.split.map((v, i) => {
     const { payload } = getLetterColour({
       characterAtIndex: v,
       usersCharacterAtIndex: textInput[i],
@@ -74,20 +34,18 @@ export default function Dummy({
     return payload;
   });
 
+
   useEffect(() => {
     if (textInput.length == display.length) {
-      setArrayOfScores([
-        ...arrayOfScores,
-        { correct: randomSent.join(""), user: textInput },
-      ]);
+      getNewSentance({type:actions.GET_NEW_SENTANCE.NEW_SENTANCE})
       setTextInput("");
-      setRandomSent(dummy[Math.floor(Math.random() * dummy.length)].split(""));
-
       console.log(arrayOfScores);
     } else {
       return;
     }
   }, [textInput]);
+
+
 
   return <div style={{ display: "flex", flexDirection: "row" }}>{display}</div>;
 }
--- src/Components/Dummy/libs/actions.js
+++ src/Components/Dummy/libs/actions.js
@@ -1,8 +1,19 @@
 import {v4 as uuidv4} from "uuid"
 import {sentances} from "./data.js"
 
+const INIT_SENTANCE_STATE = { 
+  previousSentances : [ { split:["BOOT"], Length:0, joined:"BOOT" } ], 
+  currentSentance : {split:["STRAP"], Length:0, joined:"STRAP" }, 
+  totalLength: 0 } 
 
-
+export const actions = {
+    GET_NEW_SENTANCE: {
+      NEW_SENTANCE : "NEW_SENTANCE",
+      BOOTSTRAP_STATE:()=>{
+        return reduceSentanceLogic( INIT_SENTANCE_STATE , {type:"NEW_SENTANCE"})
+      }
+  }
+}
 
 export function reduceToLetterColour(state, action){
     const {characterAtIndex, usersCharacterAtIndex} = action
@@ -14,3 +25,62 @@ export function reduceToLetterColour(state, action){
           return { ...state.payload, payload : <p key= {uuidv4()} style= {style}> {characterAtIndex} </p> }
               
   }
+
+  export function reduceSentanceLogic(state, action) {
+    let { previousSentances, currentSentance} = state;
+    const { type } = action;
+  
+    switch (type) {
+      case "NEW_SENTANCE":
+        const getUniqueSentance = (history) => {
+          //Returns a unique new sentance
+          const generateNewSentance =
+            sentances[Math.floor(Math.random() * sentances.length)];
+          const reduceHistory = history.reduce((a, b) => {
+            return [...a, b.joined];
+          }, []);
+  
+          if (reduceHistory.indexOf(generateNewSentance) != -1) {
+            return getUniqueSentance(history);
+          } else {
+            return generateNewSentance;
+          }
+        };
+        previousSentances = [...previousSentances, {...currentSentance}]
+        const getNewSentance = getUniqueSentance(previousSentances);
+  
+        const totalLength = previousSentances.reduce((a,b)=>{
+          return a+b["Length"]
+        },0)
+        
+        currentSentance = {split: getNewSentance.split(""), joined:getNewSentance, Length: getNewSentance.split(" ").length }
+  
+  
+        return {previousSentances, totalLength, currentSentance };
+  
+      default:
+        return state;
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+//Object model... I think is up to date lol 
+
+  // state = {//{type:"NEW_SENTANCE"}
+//   previousSentances: [
+//     { split:[],joined: "", Length: 0 }
+
+//   ],
+//   totalLength: 0,
+//   currentSentance: { split: [], joined: "", Length:0 },
+// };
